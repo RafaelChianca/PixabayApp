0c3f3f3aa19943c9862d674c6cbb5540389220f6
Rafael Chianca <rafachianca@gmail.com>
2022-07-09 18:17:15 -0300

    🏷️ Adds image result type and mocked data for testing

---
+++ src/services/index.ts
@@ -0,0 +1,133 @@
+export interface IImage {
+  id: number;
+  pageURL: string;
+  tags: string[];
+  imageURL: string;
+  user: string;
+  width: number;
+  height: number;
+}
+
+export const mockedData: IImage[] = [
+  {
+    id: 1,
+    pageURL: '',
+    tags: [
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+      'nature',
+    ],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 2,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 3,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 4,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 5,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 6,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 7,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 8,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 9,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    id: 10,
+    pageURL: '',
+    tags: ['1, 2, 3'],
+    imageURL:
+      'https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg',
+    user: 'chianca',
+    width: 1920,
+    height: 1080,
+  },
+];
