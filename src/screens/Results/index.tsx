d6464ee215ad04cd20ae67a126d1c0ee6bf305fb
Rafael Chianca <rafachianca@gmail.com>
2022-07-10 15:20:21 -0300

    ✨ Initial implementation of image rendering #1

--- src/screens/Results/index.tsx
+++ src/screens/Results/index.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { Alert } from 'react-native';
 
 import { ResultsTemplate } from '../../components/templates';
 import { IResultsScreenProps } from '../../routes/types';
-import { IImageResult, mockedData } from '../../services';
+import { fetchImages } from '../../services';
+import { clearImageSearch, IImageItem, RootState } from '../../store';
 import * as S from './styles';
 
 export const Results: React.FC<IResultsScreenProps> = ({
@@ -10,40 +13,54 @@ export const Results: React.FC<IResultsScreenProps> = ({
   route,
 }) => {
   const [searchText, setSearchText] = useState(route?.params?.search || '');
-  const [results, setResults] = useState(mockedData);
+
+  const dispatch = useDispatch();
+  const { hits, loading } = useSelector((state: RootState) => state.image);
 
   const handleClear = () => {
-    setResults([]);
+    dispatch(clearImageSearch());
   };
 
-  const handleImagePress = (item: IImageResult) => {
+  const handleImagePress = (item: IImageItem) => {
     if (item) {
       navigation.navigate('Details', { image: item });
     }
   };
 
-  const _renderItem = ({ item }: { item: IImageResult }) => (
-    <S.ImageButton onPress={() => handleImagePress(item)}>
-      <S.ImageItem
-        source={{
-          uri: item.imageURL,
-        }}
-      />
-    </S.ImageButton>
-  );
+  const handleSearch = () => {
+    fetchImages(searchText);
+  };
+
+  useEffect(() => {
+    handleSearch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const _renderItem = ({ item }: { item: IImageItem }) => {
+    return (
+      <S.ImageButton onPress={() => handleImagePress(item)}>
+        <S.ImageItem
+          source={{
+            uri: item.webformatURL,
+          }}
+        />
+      </S.ImageButton>
+    );
+  };
 
   return (
     <ResultsTemplate
-      loading={false}
+      loading={loading}
       searchBarProps={{
         value: searchText,
         onChangeText: setSearchText,
         onClear: handleClear,
+        onSearch: handleSearch,
       }}
       listProps={{
-        data: results,
+        data: hits,
         renderItem: _renderItem,
-        keyExtractor: (item: IImageResult) => item.id.toString(),
+        keyExtractor: (item: IImageItem) => item.id.toString(),
         onEndReachedThreshold: 0.5,
         onEndReached: () => console.log('end reached'),
       }}
